dd83aa06fe3acfa0ae941c174636086d40c9d056
LUCKSHARAN <klucksharan@gmail.com>
2026-07-04 22:00:01 +0530

    new from 1st without pull

--- sample.py
+++ sample.py
@@ -6,3 +6,5 @@ print("You can modify this script to add more features.")
 print("Let's perform a simple calculation.")
 result = 5 + 3
 print(f"The result of the calculation is: {result}")
+print("Now, let's create a list of numbers.")
+numbers = [1, 2, 3, 4, 5]
